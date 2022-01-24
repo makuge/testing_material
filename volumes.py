c32218e7fda4ed2560f45b0287ee49f9415431e8
Georg Mansky-Kummert <georg.mansky-kummert@fh-campuswien.ac.at>
2022-01-24 12:19:34 +0100

    Module volumes for exercise 1.

---
+++ volumes.py
@@ -0,0 +1,8 @@
+def volume_of_pyramid(b: float, h: float) -> float:
+    """Calculates the volume of a square pyramid.
+    Uses V = 1/3 * b^2 * h.
+    :param b: Length of (square) pyramid base
+    :param h: Height of the pyramid
+    :return: Volume of the pyramid
+    """
+    return 1 / 3 * b ** 2 * h
